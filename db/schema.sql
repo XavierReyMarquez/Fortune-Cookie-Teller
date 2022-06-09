08787ba801a6cfbd54d542a5d5a0d98dc627c9fe
cassidytrbl <97814148+cassidytrbl@users.noreply.github.com>
2022-06-08 20:55:57 -0700

    changing the db name

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,5 @@
-DROP DATABASE IF EXISTS cards_db;
-CREATE DATABASE cards_db;
+DROP DATABASE IF EXISTS fortune_db;
+CREATE DATABASE fortune_db;
 
-USE cards_db;
-
-CREATE TABLE title(
-  id INT NOT NULL,
-  card_name VARCHAR(30) NOT NULL,
-);
-
-CREATE TABLE categories(
-  id INT NOT NULL,
-  category_name VARCHAR(30) NOT NULL
-);
+USE fortune_db;
 
