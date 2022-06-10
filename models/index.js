7b055a449a35ab5875b9a7e5f1b87f05ab19ecce
juliakhdavis <juliakhdavis@gmail.com>
2022-06-10 09:26:22 -0700

    js update to journal entries

--- models/index.js
+++ models/index.js
@@ -11,4 +11,13 @@ Card.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
 module.exports = { User, Card, Comment };
